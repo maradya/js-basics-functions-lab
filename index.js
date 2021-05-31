af42ba356cdf07adf09f8d3f5d502ddb7dc21af1
Marady <maradyalexis@gmail.com>
2021-05-30 21:36:14 -0400

    commit tree 2417565595a64bf19517ad16afa42888131ce1f5

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+
+function distanceTravelledInBlocks(start, end) {
+    return Math.abs(start - end);
+  }
+  
+  function distanceTravelledInFeet(start, end) {
+    return (distanceTravelledInBlocks(start, end) * 264);
+  }
+  
+  function distanceFromHqInBlocks(street) {
+    return distanceTravelledInBlocks(street, 42);
+  }
+  
+  function distanceFromHqInFeet(street) {
+    return distanceTravelledInFeet(street, 42);
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2100) {
+      return (distance - 400) * .02
+    } else if ( distance > 1584 && distance < 2500) {
+      return 25
+    } else {
+      return 'cannot travel that far'
+    }
+  }
